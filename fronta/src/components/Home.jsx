c5f2fc8e1cbc71755c913ea253948888a45e4673
malekaloui1 <malekaloui888@gmail.com>
2024-02-16 15:32:08 +0100

    navbar

--- fronta/src/components/Home.jsx
+++ fronta/src/components/Home.jsx
@@ -2,7 +2,69 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-malek bg-no-repeat bg-cover bg-center h-[20cm] relative'>
+    <div className='absolute inset-0 bg-black opacity-50 z-10'></div>
+   <div className="flex justify-between items-center z-20 ">
+   
+   <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t1.15752-9/417653800_771342754913756_515758657566049752_n.png?_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=7p6OH5hM2iwAX-0O_kb&_nc_ht=scontent.ftun9-1.fna&oh=03_AdTdDa4KmO8EAslGvXUaspUkqFuQLYWEam4vyNz-4brlZg&oe=65F6BFAE" alt="" />
+ 
+ <div className="flex space-x-8 mt-[1cm] z-20 ">
+     <h1 className="text-xl text-white font-jakarta ">HOME</h1>
+     <h1 className="text-xl text-white  font-jakarta">PROFILE</h1>
+     <h1 className="text-xl text-white  font-jakarta">ABOUT US</h1>
+     <h1 className="text-xl text-white  font-jakarta">CONTACT </h1>
+ </div>
+
+ <div className="flex space-x-4 mt-[1cm] mr-[1cm] z-20">
+     <button className="bg-[#84cc16] text-white font-popin py-2 px-6 rounded-xl">login</button>
+     <button className="border border-solid border-[#84cc16] text-[#84cc16] font-popin py-2 px-6 rounded-xl">sign up</button>
+ </div>
+</div>
+
+ 
+
+
+<div className='flex justify-center '>
+<div className='mb-[5cm] z-20'>
+<div className='flex flex-row justify-between '>
+<h1 className='text-8xl font-Anton font-black text-[132px] mt-[2cm] text-[#84cc16] z-20'>EPIC</h1>
+
+ <h1 className='text-8xl font-Anton font-black text-[132px] mt-[2cm] text-[#84cc16] z-20'>MUSIC</h1>
+ </div>
+ 
+ <h1 className='text-8xl font-Anton font-black text-[132px] text-[#84cc16] '>EXPERIENCE</h1>
+ <h1 className='text-[#3f6212] text-xl font-popin ml-[130px] '>We have the perfect music with free licensing for your </h1>
+ <h1 className='text-[#3f6212] text-xl font-popin ml-[170px] '>creativity. Discover the possibilities now!</h1>
+ 
+</div>
+
+</div>
+
+
+
+<div className='flex justify-center  rounded-[30px] mr-[13%] ml-[13%] mt-[2cm] mb-[2cm]  '>
+
+ <div className="text-center w-full l-full mb-[2cm] mt-[2cm] ">
+  <h1 className='text-white  text-4xl font-popin '>Music Downloads for All Your Creative Ventures</h1>
+  <h1 className='text-white  text-xl font-popin mb-[1cm]'>Unleash your creativity with our versatile music downloads, perfect for any project you have in mind.</h1>
+  <div className="flex justify-around">
+ <div className="flex items-center ">
+     <img className="w-8 h-8 mr-2" src="https://scontent.ftun9-1.fna.fbcdn.net/v/t1.15752-9/417946128_883814393425649_5494375753517539791_n.png?stp=cp0_dst-png&_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=n7OIp5q9MSMAX8WEV1Z&_nc_ht=scontent.ftun9-1.fna&oh=03_AdTVbntOLujEvZmhDuUtWUJXEz_0llMwtQwxRmwFmACWyQ&oe=65F6C4FE" alt="" />
+     <h1 className="text-lg text-white font-bold">Youtube</h1>
+ </div>
+ <div className="flex items-center ml-6">
+     <img className="w-8 h-8 mr-2" src="https://scontent.ftun9-1.fna.fbcdn.net/v/t1.15752-9/417946128_883814393425649_5494375753517539791_n.png?stp=cp0_dst-png&_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=n7OIp5q9MSMAX8WEV1Z&_nc_ht=scontent.ftun9-1.fna&oh=03_AdTVbntOLujEvZmhDuUtWUJXEz_0llMwtQwxRmwFmACWyQ&oe=65F6C4FE" alt="" />
+     <h1 className="text-lg text-white font-bold">Instagram</h1>
+ </div>
+ <div className="flex items-center ml-6">
+     <img className="w-8 h-8 mr-2" src="https://scontent.ftun9-1.fna.fbcdn.net/v/t1.15752-9/417946128_883814393425649_5494375753517539791_n.png?stp=cp0_dst-png&_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=n7OIp5q9MSMAX8WEV1Z&_nc_ht=scontent.ftun9-1.fna&oh=03_AdTVbntOLujEvZmhDuUtWUJXEz_0llMwtQwxRmwFmACWyQ&oe=65F6C4FE" alt="" />
+     <h1 className="text-lg text-white font-bold">Tiktok</h1>
+ </div>
+</div>
+ </div>
+</div>
+</div>
+
   )
 }
 
