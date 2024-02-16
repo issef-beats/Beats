02e97f5e8fc4bc2e4d78677fc26e841b53e15dd1
salimhanchaoui <s.hanchaoui@yahoo.fr>
2024-02-16 14:55:30 +0100

    front starter

--- fronta/src/App.jsx
+++ fronta/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from './components/Home';
-
+import Beats from './components/Beats';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
-      <Home/>
+      <Beats/>
     </div>
   );
 }
